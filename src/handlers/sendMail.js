a0a5fdf0d8394854c2a670c2fa0b402ee6e163d2
Lucas Barros <lbarros.comp@Gmail.com>
2020-10-19 11:30:21 -0300

    Inserting the .env variables

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -3,19 +3,25 @@ import AWS from 'aws-sdk';
 const ses = new AWS.SES({ region: 'us-east-1' });
 
 async function sendMail(event, context) {
+  const record = event.Records[0];
+  console.log('Record processing', record);
+
+  const email = JSON.parse(record.body);
+  const { subject, body, recipient } = email;
+
   const params = {
     Source: 'lucas.barros@sistemasgap.com.br',
     Destination: {
-      ToAddresses: ['lucas.barros@sistemasgap.com.br'],
+      ToAddresses: [recipient],
     },
     Message: {
       Body: {
         Text: {
-          Data: 'Hello from Amazon Tests :D',
+          Data: body,
         },
       },
       Subject: {
-        Data: 'Test Mail',
+        Data: subject,
       }
     },
   };
